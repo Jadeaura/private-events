417f5312fa3ff72f01bfb1e7a42c5f2ebc19043b
Keegan Provo <keeganprovo@gmail.com>
2017-04-06 22:13:15 -0600

    Separate events into past and future catagories

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,12 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: "User"
   has_many :user_event_maps, foreign_key: "attended_event_id"
   has_many :attendees, :through => :user_event_maps, source: "attending_user"
+
+  def self.upcoming_events
+    self.where("date > ?", Time.now)
+  end
+
+  def self.previous_events
+    self.where("date < ?", Time.now)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,12 @@ class User < ApplicationRecord
   has_many :events, foreign_key: "creator_id"
   has_many :user_event_maps, foreign_key: "attending_user_id"
   has_many :attended_events, :through => :user_event_maps, source: "attended_event"
+
+  def upcoming_events
+    self.events.where("date > ?", Time.now)
+  end
+
+  def previous_events
+    self.events.where("date < ?", Time.now)
+  end
 end
